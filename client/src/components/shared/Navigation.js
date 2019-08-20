c80f17f6b12ae9c8cd83251caf4ffbadde478f77
Josiah DuBose <josiah7686@gmail.com>
2019-08-20 10:28:15 -0400

    add roboto, material-ui

--- client/src/components/shared/Navigation.js
+++ client/src/components/shared/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import NavigationBar from 'react-native-navbar';
+import { Toolbar } from 'react-native-material-ui';
 import { ClickIcon } from '../shared';
 
 const Navigation = ({navigator, navButtons}) => {
@@ -23,11 +24,21 @@ const Navigation = ({navigator, navButtons}) => {
 
     const rightButtonConfig = <ClickIcon name="user" size={25} onPress={rightIconClick} />
 
-    if (navButtons) {
-        nav = <NavigationBar title={titleConfig} leftButton={leftButtonConfig} rightButton={rightButtonConfig}/>;
-    } else {
-        nav = <NavigationBar title={titleConfig}/>;
-    }
+    // if (navButtons) {
+    //     nav = <NavigationBar title={titleConfig} leftButton={leftButtonConfig} rightButton={rightButtonConfig}/>;
+    // } else {
+    //     nav = <NavigationBar title={titleConfig}/>;
+    // }
+
+    nav = <Toolbar centerElement="Title"
+        rightElement={{
+            menu: {
+                icon: "user",
+                labels: ["Profile", "Logout"]
+            }
+        }}
+        onRightElementPress={ (label) => { console.log(label) }}
+      />
 
     return (
         <View style={styles.container}>
