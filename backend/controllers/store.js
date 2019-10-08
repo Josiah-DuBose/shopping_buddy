f1b8f2b67a02198d24a4026884aeba7bbd60b4b6
Josiah DuBose <josiah7686@gmail.com>
2019-10-08 15:10:24 -0400

    working places search

--- backend/controllers/store.js
+++ backend/controllers/store.js
@@ -1,18 +1,24 @@
 const mongoose = require('mongoose');
 const utils = require('../helpers/util');
-const request = require('request');
+const _ = require('lodash');
+const googleMapsClient = require('@google/maps').createClient({
+    key: process.env.GOOGLE_API_KEY,
+    Promise: Promise
+});
 
-exports.findStores = async (name) => {
+exports.findStores = async (req) => {
     try {
-        const API_KEY = process.env.GOOGLE_API_KEY;
-        const options = {
-            uri: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${name}&inputtype=textquery&key=${API_KEY}`,
-            method: 'GET'
+       const response = await googleMapsClient.placesNearby({
+            location: [req.latitude, req.longitude],
+            keyword: req.name,
+            radius: 5000
+        }).asPromise();
+        if (_.get(response, 'json.results')) {
+            return response.json.results
+        } else {
+            throw {message: 'Store search error: Unexpected return format.'}
         }
-        const response = await request(options);
-        console.log("response", response);
-        return response;
     } catch(err) {
-        throw(utils.createError(500, 'Store search error', err));
+        throw(utils.createError(500, err.message || 'Store search error', err));
     }
 }
--- backend/routes/store.js
+++ backend/routes/store.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const storeController = require('../controllers/store');
 const isAuthenticated = require('../middlewares/auth').isAuthenticated;
 
-router.route('/find/:name').post(isAuthenticated, async (req, res, next) => {
+router.route('/find/:name/:latitude/:longitude').get(isAuthenticated, async (req, res, next) => {
     try {
-        const response = await storeController.findStores(req.params['name']);
+        const reqParams = {
+            name: req.params['name'],
+            longitude: req.params['longitude'],
+            latitude: req.params['latitude']
+        }
+        const response = await storeController.findStores(reqParams);
         res.json(response);
     } catch (err) {
         console.error(err);
